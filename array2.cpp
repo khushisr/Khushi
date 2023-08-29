1d74edaa937c8ec926c0e389894d9c344f569533
khushi srivastava <98318034+khushisr@users.noreply.github.com>
2023-08-29 19:52:22 +0530

    Add files via upload

---
+++ array2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main()
+{   
+    cout<<"cars are:";
+    string cars[5]={"BMW","Rangerover","thar","inova","wag"};
+   // for( int i=0;i<=5;i++)
+    //cout<<cars[i]<<"\t";
+    cout<<sizeof(cars);
+
+    return 0;
+}
---
+++ array4.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int array[10];
+  cout<<"enter the array";
+  for(int i=0;i<10;i++)
+{
+  cin>>array[i];
+}
+
+ cout<<"the arrays are:";
+for(int j=0;j<10;j++)
+{
+  cout<<array[j]<<"\t";
+
+}
+
+
+
+
+
+  
+  return 0;
+}
+
+
+
+
+  
